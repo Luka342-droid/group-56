4caf2e43744ace17e3d65eb63f8f779af22d4629
Luka342-droid <lamashukeli846@gmail.com>
2025-03-20 22:47:25 +0400

    Add files via upload

---
+++ lesson 48/homework/homework0.py
@@ -0,0 +1,18 @@
+# 1
+def double_char(s):
+    result = ""
+    for i in range(len(s)):
+        result += s[i] * 2
+    return result
+# 2
+def get_age(age):
+    return int(age [0])
+# 3
+def feast(beast, dish):
+    return beast[0] == dish[0] and beast[-1] == dish[-1]
+# 4
+def array_plus_array(arr1, arr2):
+    return sum(arr1) + sum(arr2)
+# 5
+def check_factor(base, factor):
+    return base % factor == 0
