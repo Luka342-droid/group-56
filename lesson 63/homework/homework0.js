6c32d7c56ac789f9a536ddf3e3c7c206acaab930
Luka342-droid <lamashukeli846@gmail.com>
2025-05-15 00:18:27 +0400

    commit

---
+++ lesson 63/homework/homework0.js
@@ -0,0 +1,24 @@
+// Task 2: Alert "Hello, world!"
+function showAlert() {
+    alert("Hello, world!");
+}
+
+// Task 3: Greet user by name in console
+function greetUser(name) {
+    console.log("Hello, " + name + "!");
+}
+
+// Task 4: Add two numbers and log the result
+function addNumbers(a, b) {
+    console.log("Sum:", a + b);
+}
+
+// Task 5: Multiply number by 10 and log the result
+function multiplyByTen(num) {
+    console.log("Result:", num * 10);
+}
+
+// Task 9: Change paragraph text when button is clicked
+function changeParagraphText() {
+    document.getElementById("myParagraph").innerText = "The text has been changed!";
+}
