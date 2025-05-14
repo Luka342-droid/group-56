22d855aa05d6b4f27df0866e4c72fdc687de5f2d
Luka342-droid <lamashukeli846@gmail.com>
2025-05-15 00:06:53 +0400

    Delete lesson 64 directory

--- lesson 64/classwork/classwork0.js
+++
@@ -1,6 +0,0 @@
-function showValue() {
-    const name = document.getElementById("name").value;
-  
-    console.log("შეყვანილია:", name);
-  }
-  
--- lesson 64/classwork/classwork1.js
+++
@@ -1,7 +0,0 @@
-function concStrings() {
-    let str1 = prompt("შეიყვანე პირველი სტრინგი:");
-    let str2 = prompt("შეიყვანე მეორე სტრინგი:");
-    let result = str1 + str2;
-    alert("შედეგი: " + result);
-  }
-  
