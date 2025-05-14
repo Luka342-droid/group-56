b3b4c3735e8895096ae34fd48b1f3ec4c317b285
Luka342-droid <lamashukeli846@gmail.com>
2025-05-15 00:00:02 +0400

    commit

---
+++ lesson 64/classwork/classwork1.js
@@ -0,0 +1,7 @@
+function concStrings() {
+    let str1 = prompt("შეიყვანე პირველი სტრინგი:");
+    let str2 = prompt("შეიყვანე მეორე სტრინგი:");
+    let result = str1 + str2;
+    alert("შედეგი: " + result);
+  }
+  
